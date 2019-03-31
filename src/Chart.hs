2ea2b5a1e93da023ad01de994f0c5639bcd0b302
parsonsmatt <parsonsmatt@gmail.com>
2019-03-31 10:07:05 -0600

    better

--- src/Chart.hs
+++ src/Chart.hs
@@ -12,18 +12,16 @@ import History
 import Lib
 
 liftProgress :: IO ()
-liftProgress = (toFile @(Layout Day Double) def "list-history.png") $ do
+liftProgress = toFile def "list-history.png" $ do
     layout_title .= "Lift History"
-    layout_x_axis `zoom` do
-        laxis_title .= "Date"
-    layout_y_axis `zoom` do
-        laxis_title .= "Weight"
+    layout_x_axis . laxis_title .= "Date"
+    layout_y_axis . laxis_title .= "Weight"
     forM_ [squat, bench press, deadlift, press] $ \liftName ->
         plot
-        $ line liftName
-        $ pure
-        $ (fmap . fmap) (maximum . fmap e1rm . concatMap (toList . liftSets) . toList)
-        $ Map.toList
-        $ onlyLiftsOf liftName
-        $ dateLifts
+        . line liftName
+        . pure
+        . (fmap . fmap) (maximum . fmap e1rm . concatMap (toList . liftSets) . toList)
+        . Map.toList
+        . onlyLiftsOf liftName
+        . dateLifts
         $ history
